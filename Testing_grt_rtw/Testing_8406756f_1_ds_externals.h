d6b928c0b6ab82f07719df8035a3af009659f8ba
Nicolò Poncia <nicolo.poncia@gmail.com>
2026-06-21 22:24:44 +0100

    commands

--- Testing_grt_rtw/Testing_8406756f_1_ds_externals.h
+++
@@ -1,4 +0,0 @@
-/* Simscape target specific file.
- * This file is generated for the Simscape network associated with the solver block 'Testing/Solver Configuration'.
- */
-/* external function headers */
